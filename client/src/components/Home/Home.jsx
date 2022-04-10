10ea18f12fda1c67816342e205ec9137d787004b
agustinrc92 <agustinrodriguezconstenla@gmail.com>
2022-04-10 12:59:29 -0300

    CSS LandingPage

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -67,55 +67,57 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <Link to="/videogame">Crear Videojuego</Link>
-      <h1>Videogames Center</h1>
+    <body>
       <div>
-        <select onChange={(e) => handleOrderByRating(e)}>
-          <option value="asc">Rating Ascendente</option>
-          <option value="desc">Rating Descendente</option>
-        </select>
-        <select onChange={(e) => handleFilterGenres(e)}>
-          <option value="All">Todos</option>
-          {genres.map((genre) => (
-            <option key={genre.id} value={genre.name}>
-              {genre.name}
-            </option>
-          ))}
-        </select>
-        <select onChange={(e) => handleFilterCreated(e)}>
-          <option value="All">Todos</option>
-          <option value="creados">Creado</option>
-          <option value="existente">Existente</option>
-        </select>
+        <Link to="/videogame">Crear Videojuego</Link>
+        <h1>Videogames Center</h1>
         <div>
-          <select onChange={(e) => handleSort(e)}>
-            <option value="desc">A-Z</option>
-            <option value="asc">Z-A</option>
+          <select onChange={(e) => handleOrderByRating(e)}>
+            <option value="asc">Rating Ascendente</option>
+            <option value="desc">Rating Descendente</option>
           </select>
-          <SearchBar />
-          <nav>
-            <Paginado
-              videogamesPerPage={videogamesPerPage}
-              videogames={videogames.length}
-              paginado={paginado}
-            />
-          </nav>
-        </div>
+          <select onChange={(e) => handleFilterGenres(e)}>
+            <option value="All">Todos</option>
+            {genres.map((genre) => (
+              <option key={genre.id} value={genre.name}>
+                {genre.name}
+              </option>
+            ))}
+          </select>
+          <select onChange={(e) => handleFilterCreated(e)}>
+            <option value="All">Todos</option>
+            <option value="creados">Creado</option>
+            <option value="existente">Existente</option>
+          </select>
+          <div>
+            <select onChange={(e) => handleSort(e)}>
+              <option value="desc">A-Z</option>
+              <option value="asc">Z-A</option>
+            </select>
+            <SearchBar />
+            <nav>
+              <Paginado
+                videogamesPerPage={videogamesPerPage}
+                videogames={videogames.length}
+                paginado={paginado}
+              />
+            </nav>
+          </div>
 
-        {currentVideoGames &&
-          currentVideoGames.map((el) => {
-            <Card name={el.name} image={el.image} genre={el.genres} />;
-            return (
-              <div>
-                <Card name={el.name} image={el.image} genre={el.genres} />
-                <Link to={`videogame/${el.id}`}>
-                  <button>Detalles</button>
-                </Link>
-              </div>
-            );
-          })}
+          {currentVideoGames &&
+            currentVideoGames.map((el) => {
+              <Card name={el.name} image={el.image} genre={el.genres} />;
+              return (
+                <div>
+                  <Card name={el.name} image={el.image} genre={el.genres} />
+                  <Link to={`videogame/${el.id}`}>
+                    <button class="buttonDetalles">Detalles</button>
+                  </Link>
+                </div>
+              );
+            })}
+        </div>
       </div>
-    </div>
+    </body>
   );
 }
--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -4,12 +4,14 @@ import "./LandingPage.css";
 
 export default function LandingPage() {
   return (
-    <div>
-      <h1>Proyecto Videogames</h1>
-      <h1>Agustin Rodriguez</h1>
-      <Link to="/home">
-        <button>Ingresar</button>
-      </Link>
-    </div>
+    <body class="body">
+      <div>
+        <h1 class="h1">Proyecto Videogames</h1>
+        <h1 class="h1">Agustin Rodriguez</h1>
+        <Link to="/home">
+          <button class="button">Ingresar</button>
+        </Link>
+      </div>
+    </body>
   );
 }
